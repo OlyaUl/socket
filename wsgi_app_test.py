53b2eb1983c27dac8b1a55f78b33f55bc82514c3
Olya <johndoe@example.com>
2017-09-28 16:48:24 +0300

    wsgi curl post

--- wsgi_app_test.py
+++ wsgi_app_test.py
@@ -7,18 +7,29 @@ from html import escape
 #from cgi import parse_qs,escape
 
 
+# def app(environ, start_response):
+#     start_response('200 OK', [('Content-Type', 'text/plain')])
+#     # return [b'Hello']
+#     #return [b'%s=%s' % (str(k).encode(), str(v).encode()) for k,v in enviror.items()]
+#     #return [('%s=%s\n' % (k,v)).encode('UTF-8') for k,v in enviror.items()]
+#     qs = parse_qs(environ['QUERY_STRING'])
+#     # a = escape(qs['a'][0])
+#     a = escape(qs.get('a', [''])[0])
+#     return [('%s=%s\n' % (k,str(v))).encode('UTF-8') for k,v in qs.items()]
+#     #return [('%s=%s\n' % (k,v and escape(v[0]))).encode('UTF-8') for k,v in qs.items()]
+
 def app(environ, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    # return [b'Hello']
-    #return [b'%s=%s' % (str(k).encode(), str(v).encode()) for k,v in enviror.items()]
-    #return [('%s=%s\n' % (k,v)).encode('UTF-8') for k,v in enviror.items()]
-    qs = parse_qs(environ['QUERY_STRING'])
-    # a = escape(qs['a'][0])
-    a = escape(qs.get('a', [''])[0])
-    # return [('%s=%s\n' % (k,str(v))).encode('UTF-8') for k,v in qs.items()]
-    #return [('%s=%s\n' % (k,v and escape(v[0]))).encode('UTF-8') for k,v in qs.items()]
-
-
+    try:
+        size = int(environ.get('CONTENT_LENGTH', 0))
+    except ValueError:
+        size = 0
+    data = environ['wsgi.input'].read(size)
+    data = parse_qs(data)
+    return [('%s=%s\n' % (k, str(v))).encode('UTF-8') for k, v in data.items()]
 
 httpserv = make_server('localhost', 8080, app)
 httpserv.serve_forever()
+
+
+#user@ST03:~/less8/web$ curl --data 'a=1&b=2' http://localhost:8080 запускать в терминале для проверки
