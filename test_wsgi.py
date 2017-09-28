630e53c73a331dd8905692067cf7879c6f4b05e7
Olya <johndoe@example.com>
2017-09-28 16:33:01 +0300

    wsgi parse QUERY_STRING

--- test_wsgi.py
+++ test_wsgi.py
@@ -2,11 +2,14 @@
 # codding = utf-8
 import wsgiref
 from wsgiref.simple_server import make_server
+from cgi import parse_qs,escape
 
 
 def app(enviror, start_response):
     start_response('200 OK', [('Content-Type', 'text/plain')])
-    return [b'Hello']
+    # return [b'Hello']
+    #return [b'%s=%s' % (str(k).encode(), str(v).encode()) for k,v in enviror.items()]
+    return [('%s=%s\n' % (k,v)).encode('UTF-8') for k,v in enviror.items()]
 
 
 httpserv = make_server('localhost', 8080, app)
